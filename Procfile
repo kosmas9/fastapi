78abcad0c9f7e2a08a8369644443254ab6d012d2
kosmas9 <34135515+kosmas9@users.noreply.github.com>
2022-04-18 11:43:41 +0300

    added heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: uvicorn app.main:app --host=0.0.0.0 --prot=${PORT:-5000}
--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-origins=["https://www.google.com"]
+origins=["*"]
 
 app.add_middleware(
     CORSMiddleware,
